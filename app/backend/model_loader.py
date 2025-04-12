b3225c443ce1906437ff3c3e6ba52cc1d69e4bfe
Aradhya <aradhyamahajan2708@gmail.com>
2025-04-12 11:26:48 +0530

    add pickle

--- app/backend/model_loader.py
+++ app/backend/model_loader.py
@@ -1,5 +1,9 @@
-import random
+# # from logistic_model import LogisticRegression  # Must be imported before unpickling
+# # import pickle
+# from main import model 
+# import numpy as np
 
-def classify_packet(features):
-    """Simulate ML classification by randomly returning 'Normal' or 'Anomalous'."""
-    return random.choice(["Normal", "Anomalous"])
+# def classify_packet(features):
+#     features = np.array([list(features.values())])  # convert dict to feature vector
+#     pred = model.predict(features)
+#     return "Anomalous" if pred[0] == 1 else "Normal"
